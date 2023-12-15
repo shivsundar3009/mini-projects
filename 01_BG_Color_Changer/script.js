ab438ef6691a9944cd14f3994bffe7e5cbb01a0e
shivsundar3009 <shivsundar3009@gmail.com>
2023-12-15 23:58:36 +0530

    project working

--- 01_BG_Color_Changer/script.js
+++ 01_BG_Color_Changer/script.js
@@ -1,8 +1,14 @@
  const boxes = document.querySelectorAll('.box')
 
+ const body = document.querySelector('body')
+
+ 
  boxes.forEach(function(box){
    box.addEventListener('click',(e)=>{
-    console.log(e.target)
-    
+     console.log(e.target.id)
+     body.style.backgroundColor = e.target.id
+     
+     
+    // body.style.backgroundColor = body.getAttribute("id")
    }) 
  })
